a3952a82d365c9c8b8cd7cab0916b0dd83181fa7
Benno Kittelmann <bkittelmann@users.noreply.github.com>
2017-10-21 16:10:25 +0200

    Add --align parameter to align branch name and status, optionally

--- branch-pr-status.py
+++ branch-pr-status.py
@@ -42,8 +42,14 @@ def print_pr_found(commit_id, last_ref_name, pull_request):
     print "%s %s '%s', %s, %s" % (commit_id, last_ref_name, pull_request.title, status, pull_request.html_url)
 
 
+def print_pr_found_minimal(commit_id, last_ref_name, pull_request, align_to = None):
+    status = "merged" if pull_request.merged else pull_request.state
+    output_format = "%%-%ss %%s" % align_to if align_to is not None else "%s %s"
+    print output_format % (last_ref_name, status)
+
+
 def print_no_pr_found(commit_id, last_ref_name):
-    print "%s %s" % (commit_id, last_ref_name)
+    print "%-50s" % last_ref_name
 
 
 def inspect_branches(args):
@@ -52,21 +58,29 @@ def inspect_branches(args):
     branch_list_command = "cd %s && git branch -l | grep -v '* ' | grep -v 'master'| xargs -n 1 -I{} git log {} -n1 --oneline --pretty=format:'%%h*%%D\n'" % args.repository
     out = check_output([branch_list_command], shell=True)
 
-    github = Github(args.user, args.token)
-
+    branches_with_latest_commits = {}
     for line in out.splitlines():
         commit_id, ref_names = line.split("*")
         ref_name_list = ref_names.split(", ")
+        branches_with_latest_commits[ref_name_list[-1]] = commit_id
 
+    align_to = None
+    if args.align:
+        longest_branch_name = sorted(branches_with_latest_commits.keys(), cmp = lambda a,b: len(b) - len(a))[0]
+        align_to = len(longest_branch_name) + 1
+
+    github = Github(args.user, args.token)
+
+    for branch, commit_id in sorted(branches_with_latest_commits.items()):
         results = github.search_issues("repo:%s %s" % (repo_name, commit_id))
         if results.totalCount > 0:
             issue = results[0]
 
             # see https://github.com/PyGithub/PyGithub/issues/572
             pull_request = issue.repository.get_pull(issue.number)
-            print_pr_found(commit_id, ref_name_list[-1], pull_request)
+            print_pr_found_minimal(commit_id, branch, pull_request, align_to)
         else:
-            print_no_pr_found(commit_id, ref_name_list[-1])
+            print_no_pr_found(commit_id, branch)
 
 
 if __name__ == '__main__':
@@ -76,6 +90,7 @@ if __name__ == '__main__':
     parser.add_argument("-u", "--user")
     parser.add_argument("-t", "--token")
 
+    parser.add_argument("--align", action="store_true", default=False)
     parser.add_argument("repository")
 
     if len(sys.argv) == 1:
