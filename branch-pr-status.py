c2a4cc9952d986ba6195a15c8ee86cb20b17c883
Benno Kittelmann <bkittelmann@users.noreply.github.com>
2017-10-22 15:03:32 +0200

    Implement three different ways to ask the user to supply username and token

--- branch-pr-status.py
+++ branch-pr-status.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
 import argparse
+import getpass
 import re
 from subprocess import check_output
 import sys
 
-from github import Github
+from github import Github, GithubException, BadCredentialsException
+import keyring
 from termcolor import colored
 
 
-description = "Check the status of pull requests originating from local branches"
+description = "Check the status of pull requests corresponding to local branches"
+
+
+try:
+    # Python 2
+    prompt = raw_input
+except NameError:
+    # Python 3
+    prompt = input
 
 
 def get_repo_name(args):
@@ -30,13 +40,6 @@ def format_status(pull_request):
     return status
 
 
-def validate_args(args):
-    if args.user == None or args.token == None:
-        print "No --user and --token arguments set"
-        return False
-    return True
-
-
 def print_pr_found(commit_id, last_ref_name, pull_request):
     status = format_status(pull_request)
     print "%s %s '%s', %s, %s" % (commit_id, last_ref_name, pull_request.title, status, pull_request.html_url)
@@ -52,7 +55,7 @@ def print_no_pr_found(commit_id, last_ref_name):
     print "%-50s" % last_ref_name
 
 
-def inspect_branches(args):
+def inspect_branches(github, args):
     repo_name = get_repo_name(args)
 
     branch_list_command = "cd %s && git branch -l | grep -v '* ' | grep -v 'master'| xargs -n 1 -I{} git log {} -n1 --oneline --pretty=format:'%%h*%%D\n'" % args.repository
@@ -69,8 +72,6 @@ def inspect_branches(args):
         longest_branch_name = sorted(branches_with_latest_commits.keys(), cmp = lambda a,b: len(b) - len(a))[0]
         align_to = len(longest_branch_name) + 1
 
-    github = Github(args.user, args.token)
-
     for branch, commit_id in sorted(branches_with_latest_commits.items()):
         results = github.search_issues("repo:%s %s" % (repo_name, commit_id))
         if results.totalCount > 0:
@@ -83,10 +84,57 @@ def inspect_branches(args):
             print_no_pr_found(commit_id, branch)
 
 
+def get_credentials():
+    credentials = keyring.get_password("branch-pr-status", "github-api-credentials")
+    if credentials:
+        user_name, token = credentials.split(":")
+        return user_name, token
+    else:
+        return None
+
+
+def store_credentials(user_name, token):
+    do_store = prompt("Login ok. Store credentials in keyring? [y/n]\n")
+    if do_store.strip().lower() == "y":
+        credentials = "%s:%s" % (user_name, token)
+        keyring.set_password("branch-pr-status", "github-api-credentials", credentials)
+
+
+def remove_credentials():
+    do_remove = prompt("Login failed. Remove credentials from keyring? [y/n]\n")
+    if do_remove.strip().lower() == "y":
+        keyring.delete_password("branch-pr-status", "github-api-credentials")
+
+
+def no_operation(*args):
+    "Does nothing"
+
+
+def login_failed():
+    sys.stderr.write("Login to GitHub API failed\n")
+
+
+def authenticate(user_name, token):
+    "Throws exception when login fails"
+    github = Github(user_name, token)
+    user = github.get_user()
+    user.login
+    return github
+
+
+def query_github(user_name, token, args, on_login_success=no_operation, on_login_failure=no_operation):
+    try:
+        github = authenticate(user_name, token)
+        on_login_success(user_name, token)
+        inspect_branches(github, args)
+    except BadCredentialsException, ex:
+        on_login_failure()
+        sys.exit(1)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
 
-    # TODO: prompt user for credentials if not given as arguments
     parser.add_argument("-u", "--user")
     parser.add_argument("-t", "--token")
 
@@ -98,5 +146,21 @@ if __name__ == '__main__':
         sys.exit(1)
     else:
         args = parser.parse_args()
-        if validate_args(args):
-            inspect_branches(args)
+
+        if args.user and args.token:
+            query_github(args.user, args.token, args, on_login_failure=login_failed)
+        else:
+            credentials = get_credentials()
+            if credentials:
+                user_name, token = credentials
+                query_github(user_name, token, args, on_login_failure=remove_credentials)
+            else:
+                user_name = prompt("Enter GitHub user name: ")
+                token = getpass.getpass(prompt="Enter personal access token: ")
+                query_github(
+                    user_name,
+                    token,
+                    args,
+                    on_login_success=store_credentials,
+                    on_login_failure=login_failed
+                )
